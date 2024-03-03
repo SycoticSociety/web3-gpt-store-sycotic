fe0a6adb403ac927c59799f22320bfeacd9920a6
Sycotic Society <thecrazydutchman2000@gmail.com>
2024-03-03 00:35:28 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider, coinbaseWallet, embeddedWallet, metamaskWallet, smartWallet } from "@thirdweb-dev/react";
+import { Cronos } from "@thirdweb-dev/chains";
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "polygon";
+const activeChain = "Cronos";
 
 const smartWalletConfig = {
   factoryAddress: "YOUR_FACTORY_ADDRESS",
