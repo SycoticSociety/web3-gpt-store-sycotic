682a8f3e2ff54dbddc92e24007ed771445c84b84
Sycotic Society <thecrazydutchman2000@gmail.com>
2024-03-03 00:22:46 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import "../styles/globals.css";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "cronos";
+const activeChain = "polygon";
 
 const smartWalletConfig = {
   factoryAddress: "YOUR_FACTORY_ADDRESS",
